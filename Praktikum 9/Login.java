b082002408579ad699a94815f69cda080ccf1bee
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-10-30 12:30:07 +0700

    praktikum 10

---
+++ Praktikum 9/Login.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        String[] usernames = {"naufal", "putra", "farhan"};
+        String[] passwords = {"000", "111", "222"};
+
+        System.out.println("******LOGIN******");
+
+        boolean login = false;
+
+        while (!login) {
+            System.out.print("Masukkan username: ");
+            String inputUsername = input11.nextLine();
+
+            System.out.println("Masukan Password: ");
+            String inputPassword = input11.nextLine();
+
+            for (int i = 0; i < usernames.length ; i++ ) {
+                if ((usernames[i].equals(inputUsername)) && (passwords[i].equals(inputPassword))) {
+                    System.out.println("Login Berhasil");
+                    login = true;
+                    break;
+            }    
+        }
+        if(!login){
+            System.out.println("Login Gagal, silahkan coba lagi!");
+        }
+
+    }
+    }
+}
+    
+
