b7e7e885fcb0f99d85622f1db1a21af080afb226
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-11-06 13:18:36 +0700

    percobaan 3

---
+++ Praktikum 11/Triangle11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Triangle11 {
+    public static void main(String[] args) {
+    Scanner input11 = new Scanner(System.in);
+    System.out.print("Masukan Nilai N: ");
+    int n = input11.nextInt();
+    int i = 0;
+    while(i<=n){
+        int j = 0;
+        while(j<i){
+            System.out.print("*");
+            j++;
+        }
+        i++;
+    }
+    }
+}
