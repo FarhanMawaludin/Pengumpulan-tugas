9d1a02a4fe1e3ba83ef1d564c1da54c3b0b6f260
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-11-06 15:08:38 +0700

    tugas individu 211

---
+++ Praktikum 11/TugasIndividu211.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TugasIndividu211 {
+    public static void main(String[] args) {
+    Scanner input11 = new Scanner(System.in);
+    System.out.print("Masukan Nilai N: ");
+    int n = input11.nextInt();
+    
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
